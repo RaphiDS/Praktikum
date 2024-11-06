5cf79931f19ed650e88062d156d1e56f115c69eb
RaphiDS <raphael.schneider@campus.lmu.de>
2024-11-06 17:10:57 +0100

    added load commands

--- skript_raphi.R
+++ skript_raphi.R
@@ -1,1 +1,5 @@
 # Skript Raphi
+load("C:/Users/49177/Desktop/Praktikum/Praktikum GIthub/StatPrak-Overdose/NSDUH_2016.RData")
+load("C:/Users/49177/Desktop/Praktikum/Praktikum GIthub/StatPrak-Overdose/NSDUH_2017.RData")
+load("C:/Users/49177/Desktop/Praktikum/Praktikum GIthub/StatPrak-Overdose/NSDUH_2018.RData")
+load("C:/Users/49177/Desktop/Praktikum/Praktikum GIthub/StatPrak-Overdose/NSDUH_2019.RData")
