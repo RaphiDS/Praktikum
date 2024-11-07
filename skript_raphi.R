6b27661672190795f83184f9233a087526e35459
RaphiDS <raphael.schneider@campus.lmu.de>
2024-11-07 14:32:06 +0100

    more vars

--- skript_raphi.R
+++ skript_raphi.R
@@ -14,13 +14,19 @@ filterdata2015 <- PUF2015_021518 %>%
            ircigage:iialcyfu, ircocage:iicocyfu, irherage:iiheryfu, cigflag:alcmon, cocflag:cocmon, herflag:hermon,
            opinmyr:illmon, illemflag:illalcflg, cigafu:ALCAFU3, alcydays, cocydays, cigmdays:alcmdays, cocmdays,
            CIG1PACK:FUALC21, FUCOC18:FUCOC21, FUHER18:FUHER21, cocneedl:hrndlrec, anyndlrec:nedcoc, HERSMOK2:HERSNIF2,
-           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk)) %>%
+           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk,
+           grskbngdly:grskbngwk, difobtcoc, difobther, APPDRGMON2, blntever:alcfmctd, coclottm:herfmctd, ircgirtb:iicglmr
+           )) %>% #ndssansp
   mutate(year = "2015") # heroin = pain reliever?, sedative?, cocaine = stimulant?
 
 filterdata2016 <- PUF2016_022818 %>%
   select(c(1:ALCBNG30D, cocever:CC30EST, herever:HR30EST, ircigrc:II2ALCRC, ircocrc:II2COCRC, irherrc:II2HERRC,
            iralcfy:II2ALCFY, ircocfy:II2COCFY, irherfy:II2HERFY, ircigfm:IIALCBNG30D, ircocfm:II2COCFM, irherfm:II2HERFM,
            ircigage:iialcyfu, ircocage:iicocyfu, irherage:iiheryfu, cigflag:alcmon, cocflag:cocmon, herflag:hermon,
+           opinmyr:illmon, illemflag:illalcflg, cigafu:ALCAFU3, alcydays, cocydays, cigmdays:alcmdays, cocmdays,
+           CIG1PACK:FUALC21, FUCOC18:FUCOC21, FUHER18:FUHER21, cocneedl:hrndlrec, anyndlrec:nedcoc, HERSMOK2:HERSNIF2,
+           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk,
+           grskbngdly:grskbngwk, difobtcoc, difobther, APPDRGMON2, blntever:alcfmctd, coclottm:herfmctd, ircgirtb:iicglmr
   )) %>%
   mutate(year = "2016")
 
@@ -28,20 +34,32 @@ filterdata2017 <- PUF2017_100918 %>%
   select(c(1:ALCBNG30D, cocever:CC30EST, herever:HR30EST, ircigrc:II2ALCRC, ircocrc:II2COCRC, irherrc:II2HERRC,
            iralcfy:II2ALCFY, ircocfy:II2COCFY, irherfy:II2HERFY, ircigfm:IIALCBNG30D, ircocfm:II2COCFM, irherfm:II2HERFM,
            ircigage:iialcyfu, ircocage:iicocyfu, irherage:iiheryfu, cigflag:alcmon, cocflag:cocmon, herflag:hermon,
-  )) %>%
+           opinmyr:illmon, illemflag:illalcflg, cigafu:ALCAFU3, alcydays, cocydays, cigmdays:alcmdays, cocmdays,
+           CIG1PACK:FUALC21, FUCOC18:FUCOC21, FUHER18:FUHER21, cocneedl:hrndlrec, anyndlrec:nedcoc, HERSMOK2:HERSNIF2,
+           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk,
+           grskbngdly:grskbngwk, difobtcoc, difobther, APPDRGMON2, blntever:alcfmctd, coclottm:herfmctd, ircgirtb:iicglmr
+  )) %>% 
   mutate(year = "2017")
 
 filterdata2018 <- PUF2018_100819 %>%
   select(c(1:ALCBNG30D, cocever:CC30EST, herever:HR30EST, ircigrc:II2ALCRC, ircocrc:II2COCRC, irherrc:II2HERRC,
            iralcfy:II2ALCFY, ircocfy:II2COCFY, irherfy:II2HERFY, ircigfm:IIALCBNG30D, ircocfm:II2COCFM, irherfm:II2HERFM,
            ircigage:iialcyfu, ircocage:iicocyfu, irherage:iiheryfu, cigflag:alcmon, cocflag:cocmon, herflag:hermon,
-  )) %>%
+           opinmyr:illmon, illemflag:illalcflg, cigafu:ALCAFU3, alcydays, cocydays, cigmdays:alcmdays, cocmdays,
+           CIG1PACK:FUALC21, FUCOC18:FUCOC21, FUHER18:FUHER21, cocneedl:hrndlrec, anyndlrec:nedcoc, HERSMOK2:HERSNIF2,
+           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk,
+           grskbngdly:grskbngwk, difobtcoc, difobther, APPDRGMON2, blntever:alcfmctd, coclottm:herfmctd, ircgirtb:iicglmr
+  )) %>% 
   mutate(year = "2018")
 
 filterdata2019 <- PUF2019_100920 %>%
   select(c(1:ALCBNG30D, cocever:CC30EST, herever:HR30EST, ircigrc:II2ALCRC, ircocrc:II2COCRC, irherrc:II2HERRC,
            iralcfy:II2ALCFY, ircocfy:II2COCFY, irherfy:II2HERFY, ircigfm:IIALCBNG30D, ircocfm:II2COCFM, irherfm:II2HERFM,
            ircigage:iialcyfu, ircocage:iicocyfu, irherage:iiheryfu, cigflag:alcmon, cocflag:cocmon, herflag:hermon,
+           opinmyr:illmon, illemflag:illalcflg, cigafu:ALCAFU3, alcydays, cocydays, cigmdays:alcmdays, cocmdays,
+           CIG1PACK:FUALC21, FUCOC18:FUCOC21, FUHER18:FUHER21, cocneedl:hrndlrec, anyndlrec:nedcoc, HERSMOK2:HERSNIF2,
+           rskcigpkd, rskhertry:rskbngwk, difgetcoc, difgether, appdrgmon:rkfqdblt, grskcigpkd, grskcocmon:grskherwk,
+           grskbngdly:grskbngwk, difobtcoc, difobther, APPDRGMON2, blntever:alcfmctd, coclottm:herfmctd, ircgirtb:iicglmr,
   )) %>%
   mutate(year = "2019")
 
