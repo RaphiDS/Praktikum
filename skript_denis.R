3ad066b6131df6970de57ddbef3d7109515b13aa
DenisOlu <Denis.Oluri@campus.lmu.de>
2024-11-11 22:43:59 +0100

    skript update

--- skript_denis.R
+++ skript_denis.R
@@ -4,5 +4,6 @@ load("C:/Users/denis/OneDrive/Desktop/RStudio/locales/StatPrak-Overdose/NSDUH_20
 load("C:/Users/denis/OneDrive/Desktop/RStudio/locales/StatPrak-Overdose/NSDUH_2018.RData")
 load("C:/Users/denis/OneDrive/Desktop/RStudio/locales/StatPrak-Overdose/NSDUH_2019.RData")
 
+load("~/StatPrak-Overdose/combi_redu_data.Rdata")
 
 
