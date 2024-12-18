7078510d618c497c4011db732e78416010f48991
saPuthiaparampil <Sa.Puthiaparampil@campus.lmu.de>
2024-12-18 13:21:17 +0100

    general demographics nearly done

--- Skripte persoenlich/sarah_demographics.R
+++ Skripte persoenlich/sarah_demographics.R
@@ -9,16 +9,44 @@ library(tidyverse)
 ## create new, cleaned dataset for 2019 for everyone to use
 data2019 <- allfilterdata %>%
   filter (year == 2019)
-n <- 56136
+
 
 allfilterdata[COLLENRLST]
 
-#--------------------------------------------------------------------------------------------------
-## Age of Survey People
-age <- data2019 %>%
-  select(AGE2)
+#------------------------------------------------------------------------------------------------------
+## AGE grouped into allocated Categories of NSDUH
+age.grouped <- data2019 %>%
+  select(catage) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "group") %>%
+  group_by(group) %>%
+  summarize(count =n()/56136)
 
+  
+ggplot(age.grouped, aes(x= group,y = count, fill = factor(group)))+
+  geom_col()+
+  scale_y_continuous(labels = scales::percent)
 
+# age in propertion
+age.ratio.levels <- data2019 %>%
+  select(CATAG2)%>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "group") %>%
+  group_by(group) %>%
+  summarize(count =n()/56136)
+
+ggplot(age.ratio.levels, aes(x = group, y = count, fill = factor(group)))+
+  geom_col()+
+  scale_y_continuous(labels = scales::percent)
+
+## ages properly distinct
+true.age <- data2019 %>%
+  select(CATAG7) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "group") %>%
+  group_by(group) %>%
+  summarize(count =n()/56136)
+
+ggplot(true.age, aes(x = group, y = count, fill = factor(group)))+
+  geom_col()+
+  scale_y_continuous(labels = scales::percent) ## improvemnt: create stacked bar plots of level 3?
 #------------------------------------------------------------------------------------------------------
 ## combine Variables to find out who was employed the last 12 Months (or self employed)
 ### categorized into yes,no or no answer/NA
@@ -67,24 +95,35 @@ ggplot(imputed.employment18, aes(x = factor(number), y = count, fill = factor(nu
   theme_minimal()
 
 #----------------------------------------------------------------------------------------------------
-## represent number of people who get health care for substance abuse
-# categorize into yes(1), no(2), don't know (94), skip (prvhlthin = 2 --> no private insurance!, 97/98)
-insurance.substance <- data2019 %>%
-  select(hltinalc,hltindrg)%>%
-  filter(hltinalc < 94 & hltindrg < 94) %>%
-  pivot_longer(cols = everything(), names_to = "variable", values_to = "values") 
+## sexual Identity
+gender <- data2019 %>%
+  select(irsex) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "gender") %>%
+  group_by(gender) %>%
+  summarize(count = n()/56136)
+  
+ggplot(gender,aes(x = gender, y = count, fill = factor(gender)))+
+  geom_col()+
+  scale_fill_manual(values = c("1" = "blue", "2" = "red"), labels = c("1" = "Male", "2" = "Female"))+
+  scale_y_continuous(labels = scales::percent)+
+  labs(title = "Gender", y = "Percentage", fill = "Identity")+
+  theme_minimal()
 
-## plot comparing the values
-ggplot(insurance.substance, aes(x = as.factor (values), fill = as.factor(values)))+
+#------------------------------------------------------------------------------------------------------
+## MArital Stauts 
+## ISSUE not in allfilterdata
+marital.status <- data2019 %>%
+  select (IRMARIT) %>%
+  filter (IRMARIT < 99)
+
+ggplot(marital.status, aes(x = IRMARIT, fill = factor(IRMARIT)))+
   geom_bar()+
-  facet_wrap( ~ variable)+
-  ggtitle("Drug and alcohol abuse covered by private Insurance")+ 
-  labs( fill = " Insurance status", x = "Status")
-
-
+  scale_fill_manual(labels = c("1" = "Married", "2" = "Widowed", "3" = "Divorced or Seperated", "4" = "Never been MArried"))+
+  labs(title = "Marital satus", fill = "Status")
 #------------------------------------------------------------------------------------------------------
 ## college enrollment(people aged 18-2count## college enrollment(people aged 18-22, enrolled in School and College)
 college.enrollment <- data2019 %>%
+## ISSUE. not in allfilterdata --> Graph is wrong
   select(collenrlst) %>%
   filter(collenrlst < 5) %>%
   pivot_longer(cols = everything(), names_to = "status", values_to = "value") %>%
@@ -100,30 +139,28 @@ ggplot(college.enrollment, aes( x = value, fill = as.factor(value)))+
   labs(fill = "status")+
   theme_minimal()
 
-hello
-
 #------------------------------------------------------------------------------------------------------
-## AIA (indian) segments, general racial background, alcohol in these regions
-Racial.Background <- data2019 %>%
-  select (NEWRACE2, eduhighcat) ## selected AI regions, racial background and education level
-  
-
 ## Plot for Eudcation level dependend on race
+## --> diskrete Farbskala besser
+Racial.Background <- data2019 %>%
+  select (NEWRACE2, eduhighcat) %>% ## selected AI regions, racial background and education level
+  filter(eduhighcat <5)       ## wert 5 streichen? --> Leute unter 17 kein Abschluss, zählen nicht
+
 
 ggplot(Racial.Background, aes(x = NEWRACE2, fill = factor(eduhighcat)))+
   geom_bar(position = "fill")+
   scale_y_continuous(labels = scales::percent) +
   theme_light()+
-  labs(title = "Education achieved by each Race")
-## Verhältnisse -- und wert 5 streichen? --> Leute unter 17 also kein Abschluss, zählen nicht
-
-
+  labs(title = "Education achieved by each Race")+
+  scale_fill_manual(labels = c())
 
 ## maybe better: Ireduhighst2: genauer Klassenabschluss also 5,6,7 ... college und weiter
 Racial.Background2 <- data2019 %>%
   select(NEWRACE2, IREDUHIGHST2) 
+
 ggplot(Racial.Background2, aes(x = NEWRACE2, fill = factor(IREDUHIGHST2)))+
   geom_bar(position = "fill")+
   theme_light()+
+ # scale_fill_manual(labels = c("1" = "5th", "2"= "6th", "3" ="7th", "4"= "8th", "5" ="9th", "6" = "10th", "7" = "11/12th grade", "8" = "GED", "9" = "some college credit", "10" = "Associate's degree", "11" = "college graduate or higher"))+
   scale_y_continuous(labels = scales::percent)+ 
   labs(title = "Class finished by each Race")
