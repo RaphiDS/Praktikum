248ddc99fa2a00916b37bb6a9190f12a212228f6
saPuthiaparampil <Sa.Puthiaparampil@campus.lmu.de>
2024-12-28 15:35:42 +0100

    noch nicht ganz fertig aber neues update über die Feiertage

--- Skripte persoenlich/Demographics-and-Drugs.R
+++ Skripte persoenlich/Demographics-and-Drugs.R
@@ -9,38 +9,7 @@ data2019 <- allfilterdata %>%
 #--------------------------------------------------------------------------------------
 
 ## kovarianz: filter arbeitslos und kokain/ mariuana/(alcohol /cigarette)
-#substanceUse.Work <- data2019 %>%
-  select(wrkdpstyr, wrkselfem, cocrec, herrec) %>%
-  filter(
-    wrkdpstyr %in% c(1, 2) | wrkselfem %in% c(1, 2),          # Check if wrkdpstyr or wrkselfem equals 1 or 2
-    cocrec %in% c(1, 2,91) & herrec %in% c(1, 2, 91) # Check if cocrec, crakrec, and herrec have valid values
-  ) %>%
-  mutate(
-    employed = if_else(wrkdpstyr == 2 | wrkselfem == 2, 2, 1),  # Create "employed" column
-    drug = case_when(                                         # Create "drug" column with multiple conditions
-      cocrec == 91 & herrec == 1 ~ "heroin",
-      cocrec %in% c(1, 2) & herrec %in% c(1, 2) ~ "both",
-      cocrec %in% c(1, 2) ~ "cocain",
-      TRUE ~ NA_character_                                    # Assign NA for rows that do not match any condition
-    )
-  ) %>%
-  filter(is.na(drug) == FALSE) %>%
-  group_by(employed, drug) %>%
-   summarize(count = n()/56136) 
-## Verhältnis bilden ----
 
-#substanceUse.Work
-#ggplot(substanceUse.Work, aes(x = drug, y = count, fill = factor(drug)))+
-  geom_col()+
-  facet_wrap(~employed)
-
-
-
-### FALSCH: macht kein sinn inhaltlich --> alles neu machen !!!
-
-ggplot(substanceUse.Work, aes(x = employed[[2]], y = n/56136 )) +     # muss n Zahl aller Befragten sein? oder nur von der Frage?
-  geom_col(aes(fill = as.factor(drug)), position = "dodge")+
-  labs(x = "Unemployed people", y = "Percentage")
 
 
 ## zu zeigen: von allen die Drogen nehmen, wie viele sind nicht angestellt?
@@ -63,7 +32,11 @@ ggplot(insurance.substance, aes(x = values, fill = as.factor(values)))+
 
 
 #----------------------------------------------------------------------------------------------------
-## Drinking in American Indian Areas (AI)
+## Drinking in American Indian Areas (AI) ---------------> do i use rbind to join AI and other Race?
+AI.Alcohol.Usage <- data2019 %>%
+  select(MAIIN102, NEWRACE2,ALCBNG30D) %>%
+  filter (ALCBNG30D < 94)%>%
+  filter (remove(ALCBNG30D == 85)) ## wrong code
 
 #----------------------------------------------------------------------------------------------------
 ## Trial Mosaikplot: Cocain and Gender
